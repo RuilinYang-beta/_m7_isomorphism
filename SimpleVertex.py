9933b711d33aef2f6944828b82b7480af87ef2fe
Ruilin Yang <r.yang-1@student.utwente.nl>
2020-04-14 12:43:52 +0200

    dummy vertex class for wk6

---
+++ SimpleVertex.py
@@ -0,0 +1,22 @@
+class SimpleVertex():
+
+    def __init__(self, graph_idx, label):
+        self.graph_idx = graph_idx
+        self.label = label
+
+
+    # def __init__(self, colornum, nb, graph_idx, label):
+    #     self.colornum = colornum
+    #     self.nb = nb
+    #     self.grahp_idx = graph_idx
+    #     self.label = label
+
+    def __repr__(self):
+        return str((self.graph_idx, self.label))
+
+    def __str__(self):
+        return "graph: {}; label: {}; colornum: {}; nb: {}".format(self.grahp_idx,
+                                                                   self.label,
+                                                                   self.colornum,
+                                                                   self.nb)
+
